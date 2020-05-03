c2c96ff35a3b0326f29a5adea04a21e7ad3a7d73
briansunter <bsunter@gmail.com>
2020-05-03 00:02:07 +0000

    Deploying to gh-pages from  @ e1ff859a10ea81ee4104268e03c8051ba14cbfe1 🚀

---
+++ precache-manifest.92f73ed315334a8e248f60273e103b07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a609e43d70d7c3ee44f1ef9d7744766",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b67e163d82066768754f",
+    "url": "/static/css/main.3ed037f3.chunk.css"
+  },
+  {
+    "revision": "418f3ef2f273f1e33ac5",
+    "url": "/static/js/2.9b2fb196.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9b2fb196.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b67e163d82066768754f",
+    "url": "/static/js/main.4281e677.chunk.js"
+  },
+  {
+    "revision": "a5f869eab54b0f0b8aa4",
+    "url": "/static/js/runtime-main.61c9a3f8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
